3f1037a14efbff8bf59ff4af925dd92de182ae5c
Gilgamesh <devnich@gmail.com>
2026-09-03 09:34:49 -0700

    Overhauled R script to use `here` library

--- scripts/labtest.R
+++ scripts/labtest.R
@@ -4,26 +4,25 @@
 library(dplyr)
 library(ggplot2)
 
+if (! require(here)) {
+    install.packages("here", repos = "https://cloud.r-project.org")
+}
+library(here)
+
 # ----------------------------------
 # Configure files and directories
 # ----------------------------------
-setwd("~/Documents/Installers/windows-setup/scripts")
-
 # Define directory locations
-dir_raw = "../data"
-dir_processed = "../data/processed"
+dir_raw = "data"
+dir_processed = "data/processed"
 
 # Check directories
-if (basename(getwd()) != "scripts") {
-    stop("Execute this script from the /scripts directory")
-}
-
-if (! dir.exists(dir_raw)) {
+if (! dir.exists(here(dir_raw))) {
     stop("/data directory does not exist")
 }
 
-if (! dir.exists(dir_processed)) {
-    dir.create(dir_processed, recursive = TRUE)
+if (! dir.exists(here(dir_processed))) {
+    dir.create(here(dir_processed), recursive = TRUE)
     print(paste("Created", dir_processed))
 }
 
@@ -31,18 +30,19 @@ if (! dir.exists(dir_processed)) {
 # dplyr
 # ----------------------------------
 # Import data
-species <- read.csv(paste(dir_raw, "species.csv", sep = "/"))
-surveys <- read.csv(paste(dir_raw, "surveys.csv", sep = "/"))
+species <- read.csv(here(dir_raw, "species.csv"))
+surveys <- read.csv(here(dir_raw, "surveys.csv"))
 
 print(paste("Species:", dim(species)))
 print(paste("Surveys:", dim(surveys)))
-#print(str(surveys))
 
+# Mutate data frame
 df <- surveys %>%
     mutate(weight_kg = weight / 1000,
            weight_lbs = weight_kg * 2.2)
 
-fname = paste(dir_processed, "mutated_surveys.csv", sep = "/")
+# Save output to CSV
+fname = here(dir_processed, "mutated_surveys.csv")
 write.csv(df, file = fname)
 print(paste("Saved output to", fname))
 
